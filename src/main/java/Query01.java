9f610724c6711356f570c459d270faf4ccc28812
AliGrp <122046419+aly120372906@users.noreply.github.com>
2023-03-12 19:10:41 +0300

    Initial commit

---
+++ src/main/java/Query01.java
@@ -0,0 +1,30 @@
+import java.sql.*;
+
+public class Query01 {
+    public static void main(String[] args) throws ClassNotFoundException, SQLException {
+
+        //1) Driver Tanimla
+        Class.forName("org.postgresql.Driver");
+        //2) Database'e Baglan
+        Connection con = DriverManager.getConnection(
+                "jdbc:postgresql://localhost:5432/jdbc",
+                "postgres",
+                "1203906");
+        //3) Statement
+        Statement st =con.createStatement();
+
+        ResultSet veri = st.executeQuery("select * from ogrenciler");
+        //System.out.println(veri);
+
+        while (veri.next()){
+            System.out.println(veri.getInt(1)
+                    +" - " + veri.getString(2)
+                    +" - " + veri.getString(3)
+                    +" - " + veri.getString(4));
+        }
+        con.close();
+        st.close();
+        veri.close();
+
+    }
+}
