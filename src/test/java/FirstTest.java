9b7e8f14b25a605fcb9c871d8c3f48a5957bce56
AliGrp <122046419+aly120372906@users.noreply.github.com>
2023-03-16 12:35:26 +0300

    FirstTest.1

--- src/test/java/FirstTest.java
+++ src/test/java/FirstTest.java
@@ -1,3 +1,22 @@
-public class FirstTest {
+import org.junit.Assert;
+import org.junit.Test;
 
+public class FirstTest {
+@Test
+    public void firstTest01(){
+    Assert.assertTrue(6<9);
+
+}
+
+@Test
+    public void firstTest02(){
+
+    Assert.assertTrue("Test Fail",17<9);
+}
+
+@Test
+    public void firstTes03(){
+    Assert.assertEquals("Sayilar esit degil","3","3");
+
+}
 }
